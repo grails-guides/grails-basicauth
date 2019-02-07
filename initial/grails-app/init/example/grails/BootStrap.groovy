1eb65b70e364e42f96772051a48a98eab8ecd4b0
niravassar <assarn@objectcomputing.com>
2019-02-07 12:04:04 -0600

    try to make initial and complete run on grails 4  NoClassDef error happening

--- initial/grails-app/init/example/grails/BootStrap.groovy
+++ initial/grails-app/init/example/grails/BootStrap.groovy
@@ -1,19 +1,13 @@
 package example.grails
 
-import grails.util.Environment
+
 import groovy.transform.CompileStatic
 
 @CompileStatic
 class BootStrap {
-    UserDataService userDataService
 
     def init = { servletContext ->
 
-        if (Environment.current == Environment.DEVELOPMENT) {
-            final String username = 'sherlock'
-            final String password = 'elementary'
-            User user = userDataService.save(username, password, true, false, false, false, )
-        }
     }
     def destroy = {
     }
